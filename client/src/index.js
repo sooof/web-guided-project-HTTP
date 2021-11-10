631d9fdb3604cb4c01394a85bfa6795925f7227e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-10 13:20:37 -0500

    lecture work-in-progress

--- client/src/index.js
+++ client/src/index.js
@@ -48,9 +48,10 @@ const App = () => {
         render={props => <Item {...props} setItems={setItems} />}
       />
       <Route path="/item-form" component={ItemForm} />
-      <Route path="/item-update/:id" 
+      {/* <Route path="/item-update/:id" 
         render={ ()=> <UpdateForm setItems={setItems}/> }
-      />
+      /> */}
+      <Route path="/item-form" component={UpdateForm} />
     </div>
   );
 };
