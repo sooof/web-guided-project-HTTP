31a4ff522143e3ee3b59a99899da076487d5959b
sooof <sooof@me.com>
2021-12-16 17:43:55 -0800

    MovieQuotes start

--- client/src/index.js
+++ client/src/index.js
@@ -1,60 +1,18 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import Home from "./components/Home";
-import ItemsList from "./components/ItemsList";
-import Item from "./components/Item";
-import ItemForm from "./components/ItemForm";
-import UpdateForm from "./components/UpdateForm";
-import "./styles.css";
-
-const App = () => {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:3333/items")
-      .then(res => {
-        setItems(res.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
-  return (
-    <div className="App">
-      <nav>
-        <h1 className="store-header">Dustin's Trinkets</h1>
-        <div className="nav-links">
-          <NavLink exact to="/item-form">
-            Add Item
-          </NavLink>
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-          <NavLink to="/item-list">Shop</NavLink>
-        </div>
-      </nav>
-
-      <Route exact path="/" component={Home} />
-      <Route
-        exact
-        path="/item-list"
-        render={props => <ItemsList {...props} items={items} />}
-      />
-      <Route
-        path="/item-list/:id"
-        render={props => <Item {...props} setItems={setItems} />}
-      />
-      <Route path="/item-form" component={ItemForm} />
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  rootElement
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>React Test</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
